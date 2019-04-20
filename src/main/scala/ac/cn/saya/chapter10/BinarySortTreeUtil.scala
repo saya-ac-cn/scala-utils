f3a4a291a817b4c12098995c58b13c8534abc369
liunengkai <saya@saya.ac.cn>
2019-04-20 21:37:46 +0800

    完成二叉排序树的查找及删除

---
+++ src/main/scala/ac/cn/saya/chapter10/BinarySortTreeUtil.scala
@@ -0,0 +1,161 @@
+package ac.cn.saya.chapter10
+
+/**
+  * @Title: BinarySortTreeUtil
+  * @ProjectName ScalaStudy
+  * @Description: TODO
+  * @Author liunengkai
+  * @Date: 2019-04-20 20:39
+  * @Description:
+  *              二叉排序树
+  */
+object BinarySortTreeUtil {
+
+  def main(args: Array[String]): Unit = {
+    val arr = Array(7,3,10,12,5,1,9)
+    val binarySortTree = new BinarySortTree
+    for (item <- arr){
+      binarySortTree.add(new BinarySortTreeNode(item))
+    }
+    binarySortTree.delNode(1)
+    binarySortTree.infixOrder()
+  }
+
+}
+
+class BinarySortTreeNode(val value:Int){
+  var left:BinarySortTreeNode = null
+  var right:BinarySortTreeNode = null
+
+  // 查找节点
+  def search(value:Int):BinarySortTreeNode={
+    if(value == this.value){
+      return this
+    }else if(value < this.value){
+      // 递归左边进行查找
+      if(this.left == null){
+        return null
+      }else{
+        return this.left.search(value)
+      }
+    }else{
+      // 递归右边进行查找
+      if(this.right == null){
+        return null
+      }else{
+        return this.right.search(value)
+      }
+    }
+  }
+
+  // 查找指定节点的父节点
+  def searchParent(value:Int):BinarySortTreeNode={
+    if((this.left != null && this.left.value == value)||
+      (this.right != null && this.right.value == value)){
+      return this
+    }else{
+      if(this.left != null && value < this.value){
+        return this.left.searchParent(value)
+      }else if(this.right != null && value > this.value){
+        return this.right.searchParent(value)
+      }else{
+        return null
+      }
+    }
+  }
+
+  // 添加方法
+  def add(node:BinarySortTreeNode):Unit={
+    if(node == null){
+      return
+    }
+    if(node.value < this.value){
+      // 插入的节点值小于左边
+      if(this.left == null){
+        this.left = node
+      }else{
+        this.left.add(node)
+      }
+    }else{
+      // 插入的节点值小于左边
+      if(this.right == null){
+        this.right = node
+      }else{
+        this.right.add(node)
+      }
+    }
+  }
+
+  // 中序遍历
+  def infixOrder():Unit = {
+    // 向左递归左子树
+    if(this.left != null){
+      this.left.infixOrder()
+    }
+    // 输出当前节点的值
+    println(s"节点信息 no:${value}")
+    // 向右遍历右子树
+    if(this.right != null){
+      this.right.infixOrder()
+    }
+  }
+}
+
+class BinarySortTree{
+  var root:BinarySortTreeNode = null
+
+  def search(value:Int):BinarySortTreeNode={
+    if(root == null){
+      return null
+    }else{
+      return root.search(value)
+    }
+  }
+
+  def searchParent(value:Int):BinarySortTreeNode={
+    if(root == null){
+      return null
+    }else{
+      return root.searchParent(value)
+    }
+  }
+
+  def delNode(value:Int):Unit={
+    if(root == null){
+      return
+    }
+    // 查找要删除的节点
+    var targetNode = search(value)
+    if(targetNode == null){
+      return
+    }
+    // 查找父节点
+    var parentNode = searchParent(value)
+    // 判断是否是叶子节点
+    if(targetNode.left == null && targetNode.right == null){
+      // 判断是左节点还是右子节点
+      if(parentNode.left != null && parentNode.left.value == value){
+        parentNode.left = null
+      }else{
+        parentNode.right = null
+      }
+    }
+  }
+
+  def add(node:BinarySortTreeNode):Unit={
+    if(root == null){
+      root = node
+    }else{
+      root.add(node)
+    }
+  }
+
+  def infixOrder():Unit={
+    if(root==null){
+      println("当前二叉树为空")
+    }else{
+      root.infixOrder()
+    }
+  }
+
+}
